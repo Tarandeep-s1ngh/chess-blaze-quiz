d44fab73894b11afd1ec180097e522e304fc5686
Tarandeep Singh <65854945+Tarandeep-s1ngh@users.noreply.github.com>
2022-07-22 23:06:08 +0530

    minor fix

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useQuiz } from "../context/QuizContext";
 
 export const Results = () => {
